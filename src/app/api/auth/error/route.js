a720e1ce1bcdc337155e7c99e520c21bb36dfd0b
Teo <75574673+sunsakis@users.noreply.github.com>
2024-10-15 15:35:38 +0300

    Delete route.js

--- src/app/api/auth/error/route.js
+++
@@ -1,7 +0,0 @@
-export async function GET(request) {
-    // Implement your error handling logic here
-    return new Response(JSON.stringify({ error: 'Authentication error' }), {
-      status: 200,
-      headers: { 'Content-Type': 'application/json' },
-    })
-  }
