fcb236eab4e1659d2ae115fea8177a26354b4d65
Teo <75574673+sunsakis@users.noreply.github.com>
2024-12-30 13:19:43 +0200

    improved skeleton

--- src/app/capital/page.js
+++ src/app/capital/page.js
@@ -103,16 +103,20 @@ export default function Home() {
           </div>
         ))}
         {isLoading && (
-          <div className="border p-4 rounded-lg mb-4 flex animate-pulse">
-            <div className="float-left sm:float-none mb-2 sm:mb-0 mr-4 w-24 h-28 flex-shrink-0 bg-gray-200 rounded"></div>
-            <div className="w-full">
-              <div className="h-6 bg-gray-200 rounded w-3/4 mb-4"></div>
-              <div className="h-4 bg-gray-200 rounded w-full mb-2"></div>
-              <div className="h-4 bg-gray-200 rounded w-full mb-2"></div>
-              <div className="h-4 bg-gray-200 rounded w-2/3"></div>
-              <div className="h-4 bg-gray-200 rounded w-1/4 mt-4"></div>
-            </div>
-          </div>
+          <>
+            {[1, 2, 3].map((item) => (
+              <div key={item} className="border p-4 rounded-lg mb-4 flex animate-pulse">
+                <div className="float-left sm:float-none mb-2 sm:mb-0 mr-4 w-24 h-28 flex-shrink-0 bg-gray-200 rounded"></div>
+                <div className="w-full">
+                  <div className="h-6 bg-gray-200 rounded w-3/4 mb-4"></div>
+                  <div className="h-4 bg-gray-200 rounded w-full mb-2"></div>
+                  <div className="h-4 bg-gray-200 rounded w-full mb-2"></div>
+                  <div className="h-4 bg-gray-200 rounded w-2/3"></div>
+                  <div className="h-4 bg-gray-200 rounded w-1/4 mt-4"></div>
+                </div>
+              </div>
+            ))}
+          </>
         )}
         {!hasMore && <div>No more newsletters to load.</div>}
       </main>
